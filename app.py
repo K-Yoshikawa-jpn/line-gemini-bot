12cecbd48a1745436c94fe19450f418aad57a99e
K-Yoshikawa-jpn <nonyu_nonyu_nonyu@yahoo.co.jp>
2026-07-25 10:37:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def handle_message(event):
     try:
 
         response = client.models.generate_content(
-            model="gemini-2.0-flash",
+            model="gemini-3.6-flash",
             contents=user_message,
         )
 
